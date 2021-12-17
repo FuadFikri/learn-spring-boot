078bf50131a45326b37c58bc1a2d815fc7c222ac
fuad_fikri <fuadfikri01@gmail.com>
2021-12-18 06:03:08 +0700

    database locking : test get number using multithreading

--- database-locking/src/test/java/com/fikri/syamsudin/databaselocking/OrderNumberTest.java
+++ database-locking/src/test/java/com/fikri/syamsudin/databaselocking/OrderNumberTest.java
@@ -1,6 +1,5 @@
 package com.fikri.syamsudin.databaselocking;
 
-import com.fikri.syamsudin.databaselocking.entity.OrderNumber;
 import com.fikri.syamsudin.databaselocking.service.OrderNumberService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,9 +13,30 @@ public class OrderNumberTest {
     private OrderNumberService orderNumberService;
 
     @Test
-    public void getNumberTest(){
+    public void getNumberTest() {
         Long result = orderNumberService.getNumber("21L00");
         Assertions.assertNotNull(result);
-        System.out.println("order number result = "+ result);
+        System.out.println("order number result = " + result);
+    }
+
+
+    @Test
+    public void generateNumberMultiThread() throws InterruptedException {
+        int jumlahThread = 2;
+
+
+        for (int i = 0; i < jumlahThread; i++) {
+
+
+            Thread t = new Thread(() -> {
+
+                Long result = orderNumberService.getNumber("21L00");
+                System.out.println("order number result = " + result);
+
+            });
+            t.start();
+        }
+        Thread.sleep(10_000);
+
     }
 }
