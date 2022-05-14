f7feae50545d300c0fa1a50d8588878c9497cec2
fikri <fuadfikri01@gmail.com>
2022-05-14 10:09:42 +0700

    add data migration

---
+++ filter-jpa-specification/src/main/resources/db/migration/V3__insert_book_tags_data.sql
@@ -0,0 +1,14 @@
+insert into book_store.`book_tags` (`book_id`, `tags`)  values
+(1,'murah'),
+(1,'diskon'),
+(1,'promo lebaran'),
+(2,'promo tahun baru'),
+(2,'murah'),
+(3,'cuci gudang'),
+(3,'stok terakhir')
+(3,'murah'),
+(3,'promo natal'),
+(4,'tahun ajaran baru'),
+(4,'kembali ke sekolah'),
+(4,'diskon'),
+(5,'promo');
